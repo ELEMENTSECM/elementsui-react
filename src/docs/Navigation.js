1cbbdb1f023d3354b556d29398e9cd78cefd2c4a
Denis Kashkovsky <dannie.k@me.com>
2018-03-05 14:14:28 +0200

    Revert "Component filtering in the documentation view"

--- src/docs/Navigation.js
+++ src/docs/Navigation.js
@@ -1,43 +1,19 @@
-import * as React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import ComponentFilter from './ComponentFIlter';
 
-class Navigation extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			components: this.props.components
-		};
-
-		this.filterComponents = this.filterComponents.bind(this);
-	}
-
-	filterComponents(filter) {
-		this.setState({
-			components: this.props.components.filter(
-				x => x.toLowerCase().indexOf(filter.toLowerCase()) !== -1
-			)
-		});
-	}
-
-	render() {
-		return (
-			<div className="left-menu">
-				<ComponentFilter onChange={this.filterComponents} />
-				<ul className="navigation">
-					{this.state.components.map(name => {
-						return (
-							<li key={name}>
-								<a href={`#${name}`}>{name}</a>
-							</li>
-						);
-					})}
-				</ul>
-			</div>
-		);
-	}
-}
+const Navigation = ({ components }) => {
+	return (
+		<ul className="navigation">
+			{components.map((name) => {
+				return (
+					<li key={name}>
+						<a href={`#${name}`}>{name}</a>
+					</li>
+				);
+			})}
+		</ul>
+	);
+};
 
 Navigation.propTypes = {
 	components: PropTypes.array.isRequired
