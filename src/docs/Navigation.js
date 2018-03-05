cdbb7dfefae5b92282fbf7adb85aa3b60cd20395
Denis Kashkovsky <macbook@DKASHKOVSKYI-MAC.local>
2018-03-05 12:39:43 +0200

    Docs: component filtering

--- src/docs/Navigation.js
+++ src/docs/Navigation.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import * as React from 'react';
 import PropTypes from 'prop-types';
+import ComponentFilter from './ComponentFIlter';
 
-const Navigation = ({ components }) => {
-	return (
-		<ul className="navigation">
-			{components.map((name) => {
-				return (
-					<li key={name}>
-						<a href={`#${name}`}>{name}</a>
-					</li>
-				);
-			})}
-		</ul>
-	);
-};
+class Navigation extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			components: this.props.components
+		};
+
+		this.filterComponents = this.filterComponents.bind(this);
+	}
+
+	filterComponents(filter) {
+		this.setState({
+			components: this.props.components.filter(
+				(x) => x.toLowerCase().indexOf(filter.toLowerCase()) !== -1
+			)
+		});
+	}
+
+	render() {
+		return (
+			<div className="left-menu">
+				<ComponentFilter onChange={this.filterComponents} />
+				<ul className="navigation">
+					{this.state.components.map((name) => {
+						return (
+							<li key={name}>
+								<a href={`#${name}`}>{name}</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
+}
 
 Navigation.propTypes = {
 	components: PropTypes.array.isRequired
