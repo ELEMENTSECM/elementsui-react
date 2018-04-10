c8aa30b980c6fe8bac2ba1a21534dfc0ae2d9b7b
Denis Kashkovsky <macbook@DKASHKOVSKYI-MAC.local>
2018-04-10 16:15:48 +0300

    Fix Login tests

--- src/components/Authentication/Login/Login.spec.js
+++ src/components/Authentication/Login/Login.spec.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Login from './index'
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from './index';
 
 describe('Login', () => {
 	test('displays correctly', () => {
-		const wrapper = shallow(<Login />);
+		const wrapper = shallow(
+			<Login
+				appConfig={{
+					configServerUrl: 'configServerUrl',
+					configServerAuth: 'configServerAuth',
+					configServerReq: 'configServerReq',
+					idpClient: 'idpClient',
+					baseUri: 'baseUri',
+					idpRedirectUri: 'idpRedirectUri',
+					extSystemName: 'extSystemName'
+				}}
+				actions={{
+					loginAsync: () => {},
+					logout: () => {}
+				}}
+			/>
+		);
 		expect(wrapper).toMatchSnapshot();
 	});
 });
