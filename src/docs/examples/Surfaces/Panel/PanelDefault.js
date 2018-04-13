4f9c4e91bdc23f3aaf5cb8c95da888ba731a920e
Nicklas Pouey-Winger <winger.nicklas@gmail.com>
2018-04-13 10:59:09 +0200

    Panel example and cleanup linting

--- src/docs/examples/Surfaces/Panel/PanelDefault.js
+++ src/docs/examples/Surfaces/Panel/PanelDefault.js
@@ -2,42 +2,43 @@ import * as React from 'react';
 import Panel from 'elementsui-react/Surfaces/Panel';
 import { ChoiceGroup } from 'office-ui-fabric-react/lib/ChoiceGroup';
 import Button from 'elementsui-react/Inputs/Button';
+import { PrimaryButton, DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
-export default class componentName extends React.Component {
+export default class PanelDefault extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { isOpen: false };
 	}
 
-	closePanel() {
-		alert('uuu');
-	}
-	openPanel(e) {
-		this.setState;
-	}
-	_onRenderFooterContent = function(closePanel) {
+	_onRenderFooterContent = () => {
 		return (
 			<div>
-				{/* <PrimaryButton
-					onClick={e => {
-						//e.showPanel = false;
-						this.closePanel.bind(this);
-					}}
-					style={{ marginRight: '8px' }}>
+				<PrimaryButton onClick={this.closePanel} style={{ marginRight: '8px' }}>
 					Save
 				</PrimaryButton>
-				<DefaultButton>Cancel</DefaultButton> */}
+				<DefaultButton onClick={this.closePanel}>Cancel</DefaultButton>
 			</div>
 		);
 	};
+
+	closePanel = () => {
+		const currentState = this.state.isOpen;
+		this.setState(() => ({ isOpen: !currentState }));
+	};
+
+	openPanel = () => {
+		const currentState = this.state.isOpen;
+		this.setState(() => ({ isOpen: !currentState }));
+	};
+
 	render() {
 		return (
 			<div>
 				<Panel
 					htmlId="defaultPanel"
 					headerText="Default panel"
-					isOpen={false}
-					onRenderFooterContent={this._onRenderFooterContent.bind()}
+					isOpen={this.state.isOpen}
+					onRenderFooterContent={this._onRenderFooterContent}
 					element={
 						<ChoiceGroup
 							options={[
@@ -67,7 +68,7 @@ export default class componentName extends React.Component {
 						/>
 					}
 				/>
-				<Button onClick={this.openPanel} label="Open" />
+				<Button onClick={this.openPanel} label="Open Panel" />
 			</div>
 		);
 	}
