1be3c841a624ea8e28ce26ef6f1f686879dc9daf
Denis Kashkovsky <macbook@DKASHKOVSKYI-MAC.local>
2018-04-24 18:05:54 +0300

    Use semantic colors

--- src/components/_Styling/withTheme/withTheme.spec.js
+++ src/components/_Styling/withTheme/withTheme.spec.js
@@ -6,9 +6,10 @@ import withTheme from './withTheme';
 describe('withTheme', () => {
 	test('Should render Button with theme applied', () => {
 		const StyledButton = withTheme(Button, {
-			neutralLighter: 'red',
-			neutralSecondary: 'yellow',
-			tealLight: 'red'
+			buttonBackground: 'blue',
+			buttonBackgroundHovered: 'red',
+			buttonText: 'white',
+			buttonTextHovered: 'teal'
 		});
 		const wrapper = shallow(
 			<StyledButton htmlId="defaultBtn" label="Default button" isPrimary={false} />
