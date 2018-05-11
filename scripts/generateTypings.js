36d760683cdf0e026e4de32529c5709ae7ff1007
Denys Kashkovskyi <dka@gecko.no>
2018-05-11 13:03:29 +0300

    Tests for typing generation

--- scripts/generateTypings.js
+++ scripts/generateTypings.js
@@ -1,15 +1,11 @@
 const path = require('path');
-const generator = require('react-to-typescript-definitions');
 const chokidar = require('chokidar');
 const utils = require('../bin/utils');
-const exec = require('child_process').exec;
-const chalk = require('chalk');
+const func = require('./typingsFunctions');
 
 const components = path.join(__dirname, '../src', 'components');
 
 const enableWatchMode = process.argv.slice(2) === '--watch';
-
-let moduleExports = [];
 
 if (enableWatchMode) {
 	chokidar.watch([components]).on('change', function(event, path) {
@@ -19,36 +15,21 @@ if (enableWatchMode) {
 	generateTypings(components);
 }
 
-function generateTypings(folder) {
+function generateTypings(folder, moduleExports = []) {
 	return utils
 		.getDirectories(folder)
 		.filter(x => !x.startsWith('_'))
 		.map(x => {
-			const children = generateTypings(path.join(folder, x));
+			const children = generateTypings(path.join(folder, x), moduleExports);
 
 			if (!children || children.length === 0) {
 				const filePath = path.join(folder, x, x + '.js');
-				exec(`react2dts --file ${filePath} --module-name ${x}`, (error, stdout, stderr) => {
-					if (error !== null) {
-						console.log(chalk.red(`exec error: ${error}`));
-					}
-
-					let res = `import * as React from 'react';
-
-${stdout
-						.replace(/.*declare module '\w+'\s*{\s*import \* as React from 'react';\s*/, '')
-						.replace(/.*export default \w+;/, '')
-						.replace('export const', 'declare const')
-						.slice(0, -6)}`;
-					if (res.indexOf('declare const') !== -1) {
-						res += `export default ${x};`;
-					}
-					const relPath = folder.substring(folder.indexOf('components') + 11, folder.length);
-
-					utils.writeFile(path.join(folder, x, 'index.d.ts'), res);
-					moduleExports.push(`export { default as ${x} } from './${relPath}/${x}';
-`);
-					utils.writeFile(path.join(components, 'index.d.ts'), moduleExports.join(''));
+				return func.generateType({
+					root: components,
+					filePath,
+					component: x,
+					folder,
+					moduleExports
 				});
 			}
 		});
