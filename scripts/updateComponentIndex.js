9cd3c994528159ce6470982b528fcb0efe3c77a7
Kashkovsky <dannie.k@me.com>
2018-07-10 00:18:12 +0300

    Add bootstrap exports

--- scripts/updateComponentIndex.js
+++ scripts/updateComponentIndex.js
@@ -11,6 +11,9 @@ exportStrings.push(`export { default as en } from 'react-intl/locale-data/en';`)
 exportStrings.push(`export { default as nb } from 'react-intl/locale-data/nb';`);
 exportStrings.push(`export { default as nn } from 'react-intl/locale-data/nn';`);
 exportStrings.push(`export { default as sv } from 'react-intl/locale-data/sv';`);
+exportStrings.push(
+	`export { Alert as BSAlert, Badge as BSBadge, Breadcrumb as BSBreadcrumb, Button as BSButton, ButtonGroup as BSButtonGroup, ButtonToolbar as BSButtonToolbar, DropdownButton as BSDropdownButton, ToggleButtonGroup as BSToggleButtonGroup, Carousel as BSCarousel, SplitButton as BSSplitButton, MenuItem as BSMenuItem, Clearfix as BSClearfix, FormGroup as BSFormGroup, ControlLabel as BSControlLabel, FormControl as BSFormControl, HelpBlock as BSHelpBlock, Glyphicon as BSGlyphicon, Image as BSImage, Thumbnail as BSThumbnail, Jumbotron as BSJumbotron, Label as BSLabel, ListGroup as BSListGroup, ListGroupItem as BSListGroupItem, Modal as BSModal, Nav as BSNav, NavItem as BSNavItem, NavDropdown as BSNavDropdown, Navbar as BSNavbar, Overlay as BSOverlay, Tooltip as BSTooltip, PageHeader as BSPageHeader, Pager as BSPager, Panel as BSPanel, Popover as BSPopover, ProgressBar as BSProgressBar, Table as BSTable, Tabs as BSTabs, Tab as BSTab, OverlayTrigger as BSOverlayTrigger, Well as BSWell, Grid as BSGrid, Row as BSRow, Col as BSCol, Media as BSMedia, InputGroup as BSInputGroup, InputGroupAddon as BSInputGroupAddon } from 'react-bootstrap';`
+);
 utils.writeFile(indexPath, exportStrings.join('\r'));
 
 function getExportStrings(filePath, relativePath = '.') {
