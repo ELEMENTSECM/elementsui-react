598ee3ffe6bd75ad24c79070156a7d1f66cb1ff1
Denis Kashkovsky <dannie.k@me.com>
2019-02-19 11:54:47 +0200

    Remove react-bootstrap re-exports

--- scripts/generateTypings.js
+++ scripts/generateTypings.js
@@ -52,7 +52,7 @@ function generateTypings(folder) {
 
 			utils.writeFile(path.join(folder, x, "index.d.ts"), res);
 			moduleExports.push(`export { default as ${x}, ${x}Props } from './${relPath}/${x}';`);
-			utils.writeFile(path.join(components, "index.d.ts"), moduleExports.sort().join(""));
+			utils.writeFile(path.join(components, "index.d.ts"), moduleExports.join(""));
 		}
 	});
 }
@@ -63,8 +63,6 @@ function addInitializeIcons() {
 	moduleExports.push(`export { default as nb } from 'react-intl/locale-data/nb';`);
 	moduleExports.push(`export { default as nn } from 'react-intl/locale-data/nn';`);
 	moduleExports.push(`export { default as sv } from 'react-intl/locale-data/sv';`);
-	moduleExports.push(
-		`export { Alert as BSAlert, Badge as BSBadge, Breadcrumb as BSBreadcrumb, Button as BSButton, ButtonGroup as BSButtonGroup, ButtonToolbar as BSButtonToolbar, DropdownButton as BSDropdownButton, ToggleButtonGroup as BSToggleButtonGroup, Carousel as BSCarousel, SplitButton as BSSplitButton, MenuItem as BSMenuItem, Clearfix as BSClearfix, FormGroup as BSFormGroup, ControlLabel as BSControlLabel, FormControl as BSFormControl, HelpBlock as BSHelpBlock, Glyphicon as BSGlyphicon, Image as BSImage, Thumbnail as BSThumbnail, Jumbotron as BSJumbotron, Label as BSLabel, ListGroup as BSListGroup, ListGroupItem as BSListGroupItem, Modal as BSModal, Nav as BSNav, NavItem as BSNavItem, NavDropdown as BSNavDropdown, Navbar as BSNavbar, Overlay as BSOverlay, Tooltip as BSTooltip, PageHeader as BSPageHeader, Pager as BSPager, Panel as BSPanel, Popover as BSPopover, ProgressBar as BSProgressBar, Table as BSTable, Tabs as BSTabs, Tab as BSTab, OverlayTrigger as BSOverlayTrigger, Well as BSWell, Grid as BSGrid, Row as BSRow, Col as BSCol, Media as BSMedia, InputGroup as BSInputGroup, InputGroupAddon as BSInputGroupAddon, Checkbox as BSCheckbox, Dropdown as BSDropdown } from 'react-bootstrap';`
-	);
+
 	utils.writeFile(path.join(components, "index.d.ts"), moduleExports.join(""));
 }
--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -1,68 +1,18 @@
-export { default as DateTimePicker, DateTimePickerProps } from "./Pickers/DateTimePicker";
+export { default as InfiniteList, InfiniteListProps } from "./Content/InfiniteList";
+export { default as Items, ItemsProps } from "./Content/Items";
+export { default as ListItem, ListItemProps } from "./Content/ListItem";
+export { default as Popup, PopupProps } from "./Content/Popup";
 export { default as Dropdown, DropdownProps } from "./Controls/Dropdown";
 export { default as DropdownItem, DropdownItemProps } from "./Controls/DropdownItem";
 export { default as DropdownMenu, DropdownMenuProps } from "./Controls/DropdownMenu";
 export { default as DropdownToggle, DropdownToggleProps } from "./Controls/DropdownToggle";
-export { default as InfiniteList, InfiniteListProps } from "./Content/InfiniteList";
-export { default as Items, ItemsProps } from "./Content/Items";
-export { default as ListItem, ListItemProps } from "./Content/ListItem";
+export { default as Spinner, SpinnerProps } from "./Indicators/Spinner";
+export { default as RichText, RichTextProps } from "./Inputs/RichText";
+export { default as DateTimePicker, DateTimePickerProps } from "./Pickers/DateTimePicker";
 export { default as Lookup, LookupProps } from "./Pickers/Lookup";
 export { default as LookupDialog, LookupDialogProps } from "./Pickers/LookupDialog";
-export { default as Popup, PopupProps } from "./Content/Popup";
-export { default as RichText, RichTextProps } from "./Inputs/RichText";
-export { default as Spinner, SpinnerProps } from "./Indicators/Spinner";
 export { addLocaleData } from "react-intl";
 export { default as en } from "react-intl/locale-data/en";
 export { default as nb } from "react-intl/locale-data/nb";
 export { default as nn } from "react-intl/locale-data/nn";
 export { default as sv } from "react-intl/locale-data/sv";
-export {
-	Alert as BSAlert,
-	Badge as BSBadge,
-	Breadcrumb as BSBreadcrumb,
-	Button as BSButton,
-	ButtonGroup as BSButtonGroup,
-	ButtonToolbar as BSButtonToolbar,
-	DropdownButton as BSDropdownButton,
-	ToggleButtonGroup as BSToggleButtonGroup,
-	Carousel as BSCarousel,
-	SplitButton as BSSplitButton,
-	MenuItem as BSMenuItem,
-	Clearfix as BSClearfix,
-	FormGroup as BSFormGroup,
-	ControlLabel as BSControlLabel,
-	FormControl as BSFormControl,
-	HelpBlock as BSHelpBlock,
-	Glyphicon as BSGlyphicon,
-	Image as BSImage,
-	Thumbnail as BSThumbnail,
-	Jumbotron as BSJumbotron,
-	Label as BSLabel,
-	ListGroup as BSListGroup,
-	ListGroupItem as BSListGroupItem,
-	Modal as BSModal,
-	Nav as BSNav,
-	NavItem as BSNavItem,
-	NavDropdown as BSNavDropdown,
-	Navbar as BSNavbar,
-	Overlay as BSOverlay,
-	Tooltip as BSTooltip,
-	PageHeader as BSPageHeader,
-	Pager as BSPager,
-	Panel as BSPanel,
-	Popover as BSPopover,
-	ProgressBar as BSProgressBar,
-	Table as BSTable,
-	Tabs as BSTabs,
-	Tab as BSTab,
-	OverlayTrigger as BSOverlayTrigger,
-	Well as BSWell,
-	Grid as BSGrid,
-	Row as BSRow,
-	Col as BSCol,
-	Media as BSMedia,
-	InputGroup as BSInputGroup,
-	InputGroupAddon as BSInputGroupAddon,
-	Checkbox as BSCheckbox,
-	Dropdown as BSDropdown
-} from "react-bootstrap";
