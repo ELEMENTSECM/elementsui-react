3642b42e9d889250741564be4b7f0d6a9615ccb4
Dmitry Marcautan <dmitry.marcautan@evry.com>
2019-11-25 12:26:33 +0200

    Fix tests

--- src/InfiniteList/__tests__/InfiniteList.spec.tsx
+++ src/InfiniteList/__tests__/InfiniteList.spec.tsx
@@ -5,9 +5,9 @@ import InfiniteList from "..";
 describe("InfiniteList", () => {
 	test("displays correctly", () => {
 		const wrapper = shallow(
-			<InfiniteList locale="en" dataLength={20} hasMore={false}>
-				{[...Array(20)].map((_, i) => {
-					return <div key={i}>Item {i}</div>;
+			<InfiniteList dataLength={20} hasMore={false}>
+				{new Array(20).fill(null).map((_, i) => {
+					return <InfiniteList.Item key={i}>Item {i}</InfiniteList.Item>;
 				})}
 			</InfiniteList>
 		);
